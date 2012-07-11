7efc369c319df3d15ed66ce4779fff3b081e80c7
Thierry Forest <thierry.forest@univ-angers.fr>
2012-07-11 11:24:58 +0200

    myldap completed , mycas to be completed

--- app/models/mycas.rb
+++ app/models/mycas.rb
@@ -1,3 +1,38 @@
+require 'rubygems'
+require 'casclient'
+require 'casclient/frameworks/rails/filter'
+require 'net/ldap'
+
 class Mycas < ActiveRecord::Base
   unloadable
+
+  #???
+           has_many :users
+
+  #???
+       validates_presence_of :url,:ldap,:dn
+  # to redefine ??
+  def authenticate(controller)
+    init_client
+      CASClient::Frameworks::Rails::Filter.filter(controller)
+  end
+
+  def is_staff(login)
+
+  end
+
+  def onthefly(login)
+
+  end
+
+  def logout
+
+  end
+
+private
+
+  def init_client
+       CASClient::Frameworks::Rails::Filter.configure(:cas_base_url => self.url)
+       return CASClient::Frameworks::Rails::Filter.client
+  end
 end
--- app/models/myldap.rb
+++ app/models/myldap.rb
@@ -1,3 +1,42 @@
+require 'net/ldap'
+
 class Myldap < ActiveRecord::Base
   unloadable
+
+
+  def get_data
+    # Création de la collection data (hashset)
+    data = {
+        "username"=>"auth_username",
+        "password"=>"auth_password",
+        "base"=>"auth_base",
+        "host"=>"ldap_host",
+        "port"=>"ldap_port",
+        "field"=>"filter_field",
+        "value"=>"filter_value",
+    }
+    # Recupération des valeurs depuis la base de donnée
+    data.each {
+        |key,value|
+      data["#{key}"] = Setting.plugin_redmine_clruniv["#{value}"]
+    }
+    @my_data=data
+  end
+
+  def get_connect
+    #Initialisation de la connexion
+    auth = {:method => :simple, :username => @my_data["username"], :password => @my_data["password"]}
+    @ldap = Net::LDAP::new :host => @my_data["host"], :port => @my_data["port"], :auth => auth
+  end
+
+  def set_filter
+    #Définition du filtre
+    @filter = Net::LDAP::Filter.eq(@my_data["field"], @my_data["value"])
+  end
+
+  def get_result
+
+    #Récupération du résultat
+    @result = @ldap.search(:base  => @my_data["base"], :filter => @filter  )
+    end
 end
