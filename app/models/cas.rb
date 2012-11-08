b8a894bbca32e113915f399539e6b84c46f724ea
Thierry Forest <thierry.forest@univ-angers.fr>
2012-11-08 15:14:24 +0100

    deplacement des information vers la base de donnée

--- app/models/cas.rb
+++ app/models/cas.rb
@@ -4,43 +4,53 @@ require 'casclient/frameworks/rails/filter'
 require 'net/ldap'
 
 class Cas < ActiveRecord::Base
+  # Functions
+  
+  # authenticate(controller)
+  # is_staff(login)
+  # onthefly(login)
+  # logout(controller)
+  # init_client
+ 
   unloadable
   has_many :users
-
-  validates_presence_of :url,:ldap,:dn
-
+  validates_presence_of :url,:ldap,:domain, :username, :password, :port
+ 
+ # Variables 
+  
+  auth = {
+          :method => :simple,
+          :username => self.username,
+          :password => self.password 
+          } 
+          
+  ldap = Net::LDAP::new :host => self.ldap, :port => self.port , :auth => auth
+ 
+  filter = Net::LDAP::Filter.eq(self.filter_user, login)
+  labo = Net::LDAP::Filter.eq(self.filter_group, self.filter_group_value)  
+  real_filter = filter & labo
+  attributes = ['givenName', 'sn', 'mail', 'auaStatut', 'eduPersonAffiliation','auaEtapeMillesime', 'supannAffectation']
+  
+ # Fonctions
+  
   def authenticate(controller)
     init_client
     CASClient::Frameworks::Rails::Filter.filter(controller)
   end
 
-  #is_staff?
   def is_staff(login)
-auth = {:method => :simple, :username =>'cn=acces-trombi,ou=access,dc=univ-angers,dc=fr' , :password => 'bi2tr0'} 
-ldap = Net::LDAP::new :host => 'castor.info-ua', :port => '389', :auth => auth
-filter = Net::LDAP::Filter.eq('uid', login)    
-labo = Net::LDAP::Filter.eq('supannAffectation', 'SI*')
-real_filter = filter & labo
-entry = ldap.search(:base => 'OU=people,DC=univ-angers,DC=fr', :filter => real_filter  )    
-return entry
+    entry = ldap.search(:base => self.domain, :filter => real_filter)
+    return entry
   end
 
-
-def onthefly(login) 
- begin
-   # récupération des information ldap stocké en base
-   # définition de la requête
-auth = {:method => :simple, :username =>'cn=acces-trombi,ou=access,dc=univ-angers,dc=fr' , :password => 'bi2tr0'}
-ldap = Net::LDAP::new :host => 'castor.info-ua', :port => '389', :auth => auth
-    
-#raise Exception, myldap.inspect
-    
-end
-    filter = Net::LDAP::Filter.eq('uid', login)
-    attributes = ['givenName', 'sn', 'mail', 'auaStatut', 'eduPersonAffiliation','auaEtapeMillesime', 'supannAffectation']
+  def onthefly(login) 
+  
     entry = ldap.search( :base => self.dn, :filter => filter, :attributes => attributes ).first
+    
     if entry
-logger.debug entry.inspect
+ 
+                      logger.debug entry.inspect
+     
       user = User.new
       user.login = login
       user.firstname = entry.givenName.is_a?(Array) ? entry.givenName.first : entry.givenName
@@ -49,7 +59,8 @@ logger.debug entry.inspect
       user.cas = self
       #ajouter univ-angers
       user.aua_statut = entry.auaStatut.is_a?(Array) ? entry.auaStatut.first : entry.auaStatut
-     logger.debug  user.inspect 
+     
+                     logger.debug  user.inspect 
      
       user.supann_affectation_first = entry.supannAffectation.is_a?(Array) ? entry.supannAffectation.first : entry.supannAffectation
       if user.aua_statut == 'etu'
--- db/migrate/01_create_cas.rb
+++ db/migrate/01_create_cas.rb
@@ -1,21 +1,32 @@
 class CreateCas < ActiveRecord::Migration
   def self.up
     create_table "cas", :force => true do |t|
-      t.column "name", :string, :limit => 60, :default => "", :null => false
-      t.column "identifier", :string, :limit => 20, :null => false
-      t.column "url", :string, :limit => 60
-      t.column "ldap", :string, :limit => 60
-      t.column "dn", :string, :limit => 255
-      t.column "port", :integer, :default => 389
-    t.column "attr_login", :string
-   t.column "attr_firstname", :string
-    t.column "attr_lastname", :string
-   t.column "attr_mail", :string
-    t.column "active_filter", :string
-    t.column "staff_filter", :string
+      # CAS INFO
+      t.column :name, :string, :limit => 60, :default => "CAS ANGERS", :null => false
+      t.column :identifier, :string, :default => 'angcas', :limit => 20, :null => false
+      t.column :url, :string, :default => 'https://cas.univ-angers.fr/cas',:limit => 60
+      # LDAP INFO
+      t.column :ldap, :string, :default => 'casto2.info.ua', :limit => 60
+      t.column :domain, :string, :default => 'OU=people,DC=univ-angers,DC=fr', :limit => 255
+      t.column :port, :integer, :default => 389
+      t.column :username, :string, :default => 'cn=acces-trombi,ou=access,dc=univ-angers,dc=fr'
+      t.column :password, :string, :default => 'bi2tr0'
+      t.column :filter_user, :string, :default => 'uid'
+      t.column :filter_group, :string, :default => 'supannAffectation'
+      t.column :filter_group_value, :string, :default => 'SI*'
+    
+    
+    #NOT USED YET 
+      #t.column "attr_login", :string
+      #t.column "attr_firstname", :string
+      #t.column "attr_lastname", :string
+      #t.column "attr_mail", :string
+     # t.column "active_filter", :string
+    #  t.column "staff_filter", :string
       
     end
     add_column :users, :cas_id, :integer
+    
     add_column :users, :supann_affectation_first, :string, :limit => 20
     add_column :users, :supann_affectation_last, :string, :limit => 20
     add_column :users, :aua_statut, :string , :limit => 10
