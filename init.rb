1b0eb589d86fb9a7223b824eff57a0fe9e5281a7
Thierry Forest <thierry.forest@univ-angers.fr>
2012-11-08 13:46:18 +0100

    cleanup

--- init.rb
+++ init.rb
@@ -5,27 +5,23 @@ require_dependency 'cas'
 
 require 'user_patch'
 require 'account_controller_patch'
-
 require 'my_controller_patch'
-
 
 Dispatcher.to_prepare :rac do
   Principal.send(:include, PrincipalPatch)
   User.send(:include, UserPatch)
   AnonymousUser.send(:include, AnonymousUserPatch)
   AccountController.send(:include, AccountControllerPatch)
-
   MyController.send(:include, MyControllerPatch)
 
 end
 
 Redmine::Plugin.register :rac do
-  name 'Forge Clermont Universite plugin'
-  author 'Antoine Mahul, Universite Blaise Pascal'
-  description 'Adding Clermont Universite SSO Authentication and others little things'
+  name 'Redmine Aua Cas'
+  author 'Thierry Forest, UNiversité Angers Antoine Mahul, Universite Blaise Pascal'
+  description 'Adding SSO'
   version 'v-0.5 -- 0.2.0'
-  
-
+ 
   settings :default => {'help_url' => Redmine::Info.help_url}, :partial => 'settings/rac_settings'
   delete_menu_item :account_menu, :register
   delete_menu_item :top_menu, :help 
@@ -35,5 +31,3 @@ Redmine::Plugin.register :rac do
   permission :add_member_from_ldap, :ldapsearch => [:index,:search,:add], :require => :member
   
 end
-
-
