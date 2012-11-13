836867fa32c6a1bb64fdb3dcb96e74761e98df25
Thierry Forest <thierry.forest@univ-angers.fr>
2012-11-13 12:04:25 +0100

    du chemin de fixture spécifique au plugin

--- test/unit/cas_test.rb
+++ test/unit/cas_test.rb
@@ -1,14 +1,16 @@
 # cas_test.rb
 #
-
 # configuration par default
 require File.expand_path('../../test_helper', __FILE__)
 
 class CasTest < ActiveSupport::TestCase
   
   #fixtures :ldap, :cas, :user
+FIXTURES_PATH = File.join(File.dirname(__FILE__), '../fixtures')
   
-  fixtures  :users, :cas
+  def setup
+      Fixtures.create_fixtures(FIXTURES_PATH,'users')
+  end
   
   #première methode 
   def test_truth
