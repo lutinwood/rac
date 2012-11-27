011a007f9308a7e4c32c60a4f10075e02563b26e
Thierry Forest <thierry.forest@univ-angers.fr>
2012-11-27 12:16:24 +0100

    creéation des actions du controller formations

--- app/controllers/formations_controller.rb
+++ app/controllers/formations_controller.rb
@@ -9,16 +9,34 @@ class FormationsController < ApplicationController
       @formation = Formation.all
     end
     
+    #POST /formation
     def create
       @formation = Formation.new(params[:formation])
+      respond_to do |format|
+        if @formation.save
+            format.html { redirect_to(:action => 'full_update')}
+            
+        end
+      end
     end
     
+    def full_update 
+        @formations = Formation.all
+          #Nouvelle entrée
+            if params[:state].eql? "new"
+                @formation = Formation.new
+            end
+            respond_to do |format|
+                format.html # full_update.html.erb
+            end
+    end
+    
+    # PUT /formation/1
     def update 
-     #  @formation = Formation.find(params[:id])
-      @formations = Formation.all
-	respond_to do |format|
-		format.html { render :action => :new }
-	end
+     @formation = Formation.find(params[:id])
+     	  respond_to do |format|
+		      format.html { redirect_to(:action => 'full_update', :notice => 'Formation ws successfully updated. ') }
+	      end
     end
 
 	def update_formation
@@ -31,6 +49,10 @@ class FormationsController < ApplicationController
     
     def destroy 
         @formation = Formation.find(params[:id])
+         @formation.destroy
+          respond_to do |format|
+              format.html { redirect_to(:action => 'full_update')}
+            end
+          end
       
     end
-end
